d4c3b32df2bdb26491fdbf3323963a95b59f92f9
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-15 16:12:59 -0300

    Flashlight script

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public float battery = 20.0f;
+    public GameObject luz;
+
+    void Update()
+    {
+        if (luz.activeSelf)
+        {
+            battery -= Time.deltaTime;
+        }
+        if (battery <= 0)
+        {
+            luz.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.F) && battery > 0)
+        {
+            luz.SetActive(!luz.activeSelf);
+        }
+    }
+}
