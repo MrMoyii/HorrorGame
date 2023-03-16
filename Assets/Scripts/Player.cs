acd5bade35af684e1344ee6f2c31241396b180a5
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-16 10:25:31 -0300

    Power system and updates

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]private Transform camara;
-    [SerializeField]private GameManager gameManager;
+    [SerializeField] private Transform camara, flashLight;
+    [SerializeField] private GameManager gameManager;
+    [SerializeField] private CharacterController cc;
+    [SerializeField] private float energy = 10, maxEnergy = 10, timeToRecover = 5;
+    [SerializeField] private FPSController fPSController;
 
-    private float distanceRaycast = 3.0f;
+    private float distanceRaycast = 3.5f;
     void Update()
     {
         //para ver el Raycast
@@ -25,5 +28,36 @@ public class Player : MonoBehaviour
                 }
             }
         }
+
+        //sistema de energia y baja la linterna al correr
+        if (Input.GetKey(KeyCode.LeftShift) && cc.velocity.magnitude >= 1 && energy >= 0)
+        {
+            flashLight.localEulerAngles = Vector3.Lerp(flashLight.localEulerAngles, new Vector3(25, 0, 0), Time.deltaTime * 2.5f);
+
+            energy -= Time.deltaTime;
+
+            //penalizacion por consumir todo el timepo de correr
+            if (energy <= 0.1f)
+            {
+                energy = -Mathf.Abs(timeToRecover);
+                fPSController.walkSpeed = 3;
+                fPSController.runSpeed = 3;
+            }
+        }
+        else
+        {
+            flashLight.localEulerAngles = Vector3.Lerp(flashLight.localEulerAngles, new Vector3(0, 0, 0), Time.deltaTime * 2.5f);
+
+            if (energy >= 0)
+            {
+                fPSController.walkSpeed = 6;
+                fPSController.runSpeed = 10;
+            }
+
+            if (energy <= Mathf.Abs(maxEnergy))
+                energy += Time.deltaTime;
+        }
+
+        
     }
 }
