63ff1bcb5187b92143c5e875b75226ac03e7a73a
Omar Moyano <omarmoyano7w@gmail.com>
2023-03-20 17:03:05 -0300

    sounds for battery

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Battery : MonoBehaviour
 {
     public Flashlight linterna;
+    public AudioSource clip;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.transform.CompareTag("Player"))
         {
+            clip.Play();
             linterna.battery += 7.0f;
             Destroy(gameObject);
         }
